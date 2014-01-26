3ab1cfaf4e3324bdedb8a1692be3bff61c803070
akabeko <akabekobeko@gmail.com>
2014-01-26 16:36:10 +0900

    #174 Google Maps API 連携を実装

--- HostWebView/src/me/akabeko/hostwebview/GoogleMapsActivity.java
+++ HostWebView/src/me/akabeko/hostwebview/GoogleMapsActivity.java
@@ -1,6 +1,12 @@
 package me.akabeko.hostwebview;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebView;
@@ -18,6 +24,9 @@ public class GoogleMapsActivity extends Activity {
     /** ローカル HTML を表示する WebView。 */
     private WebView mWebView;
 
+    /** 住所を表示するための TextView。 */
+    private TextView mAddressTextView;
+
     /**
      * 指定された URL が WebView からのコールバックであることを調べ、対応する処理を実行します。
      *
@@ -26,18 +35,31 @@ public class GoogleMapsActivity extends Activity {
      * @return コールバックだった場合は true。
      */
     private boolean checkCallbackUrl( String url ) {
-        final String CallbacScheme = "app-callback://map?";
+        final String CallbacScheme = "app-callback://map";
         if( !url.startsWith( CallbacScheme ) ) { return false; }
+
+        Map< String, String > params = this.parseUrlParameters( url );
+        String address   = params.get( "address" );
+        String latitude  = params.get( "lat" );
+        String longitude = params.get( "lng" );
+        Log.d( "HostWebView", String.format( "address = %s, latitude = %s, longitude = %s", address, latitude, longitude ) );
+
+        if( address != null ) {
+            this.mAddressTextView.setText( address );
+        }
 
         return true;
     }
 
     /**
-     * WebView 上に読み込まれた JavaScript の関数を実行します。
+     * マップの中央位置を指定された住所の位置へ移動させます。
      *
-     * @param param JavaScript の関数へ指定するパラメータ。
+     * @param address 住所。ジオコーディングによって検索されます。
      */
-    private void executeJavaScriptFunction( String param ) {
+    private void moveToMapCenter( String address ) {
+        final String region = this.getString( R.string.google_map_rgion );
+        final String script = "javascript:window.webViewCallbackSearchAddress('%s','%s');";
+        this.mWebView.loadUrl( String.format( script, address, region ) );
     }
 
     /**
@@ -69,12 +91,41 @@ public class GoogleMapsActivity extends Activity {
                     InputMethodManager imm = ( InputMethodManager )getSystemService( INPUT_METHOD_SERVICE );
                     imm.hideSoftInputFromWindow( v.getWindowToken(), InputMethodManager.HIDE_IMPLICIT_ONLY );
                     imm.hideSoftInputFromWindow( v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS );
-                    executeJavaScriptFunction( v.getText().toString() );
+                    moveToMapCenter( v.getText().toString() );
                 }
 
                 return false;
             }
         } );
+    }
+
+    /**
+     * URL におけるパラメータ部分を解析してディクショナリ化します。
+     *
+     * @param url URL。
+     *
+     * @return 解析結果。
+     */
+    private Map< String, String > parseUrlParameters( String url ) {
+        Map< String, String > result = new HashMap< String, String >();
+        int                   index = url.indexOf( "?" );
+        if( index == -1 ) { return result; }
+
+        String[] params = url.substring( index + 1 ).split( "&" );
+        for( String param : params ) {
+            String[] keyValuePair = param.split( "=" );
+            if( keyValuePair.length >= 2 ) {
+                try {
+                    String value = URLDecoder.decode( keyValuePair[ 1 ], "utf-8" );
+                    result.put( keyValuePair[ 0 ], value );
+
+                } catch( UnsupportedEncodingException e ) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return result;
     }
 
     /**
@@ -86,6 +137,7 @@ public class GoogleMapsActivity extends Activity {
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         this.setContentView( R.layout.activity_google_maps );
+        this.mAddressTextView = ( TextView )this.findViewById( R.id.textViewAddress );
         this.initWebView();
         this.initEditText();
     }
